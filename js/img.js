eb68c8266b214572a03703a7b1d043acf11f0fde
Jingyeong <70802352+dooli1971039@users.noreply.github.com>
2022-01-24 04:45:21 +0900

    Update img.js

--- js/img.js
+++ js/img.js
@@ -2,6 +2,8 @@ const images=["0.png","1.png","2.png","3.png","4.png","5.png","6.png","7.jpg","8
 
 const chosenImage=images[Math.floor(Math.random()*images.length)];
 
-src=`img/${chosenImage}`;
- const img=document.querySelector("#mushroom");
- img.src=src;
+const left=document.querySelector("#left");
+
+const bgImage=document.createElement("img");
+bgImage.src=`img/${chosenImage}`;
+left.appendChild(bgImage);
